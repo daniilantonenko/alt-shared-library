2431fac2c4fa2af4178f1107f23a156cc258c814
Daniil Antonenko <daniilantonenko@ya.ru>
2023-07-14 22:23:17 +0300

    Dev: Reading JSON by GET request

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include<stdio.h>
+#include "main.h"
+
+using namespace std;
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string *)userp)->append((char *)contents, size * nmemb);
+    return size * nmemb;
+}
 
 int main(void)
 {
- printf("Hello! This is a test prgoram.\n");
- return 0;
+    CURL *curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://rdb.altlinux.org/api/export/branch_binary_packages/p10?arch=x86_64");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        std::cout << readBuffer << std::endl;
+    }
+    
+    return 0;
 }
